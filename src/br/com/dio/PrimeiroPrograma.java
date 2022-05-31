1a4e3df9c1c6b4d00eec1f61c215cba596dcb806
Henri-que <henrique_souza92@hotmail.com>
2022-05-31 10:07:29 -0300

    First commit

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,19 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+
+    private static Object Gato;
+
+    public static void main(String[] args) {
+
+        Gato = new Gato();
+
+        System.out.println(Gato);
+
+        /*int a = 5;
+        int b = 3;
+        System.out.println("Hello World " + (a+b));*/
+    }
+}
